0b107347f8525a9c0d9bc42565805f4574ac46b6
Fidal Mathew <fidal15perfect@gmail.com>
2025-02-04 15:30:41 +0530

    added cdp

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,8 @@
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const {communicateWithAgent, initializeAgent} = require("./cdpActions");
+import { Request as ExpressRequest } from "express";
 
 dotenv.config();
 
@@ -10,7 +12,20 @@ const PORT = process.env.PORT || 8000;
 app.use(cors());
 app.use(express.json());
 
-import { Request as ExpressRequest } from "express";
+// initializeAgent
+
+let cdpAgent: any, cdpConfig: any;
+
+const main = async () => {
+
+    const { agent, config } = await initializeAgent();
+    cdpAgent = agent;
+    cdpConfig = config;
+}
+
+main();
+
+
 
 interface Request extends ExpressRequest {
     body: {
@@ -31,6 +46,16 @@ app.post("/test", (req: Request, res: Response) => {
     console.log(req.body);
     const {prompt} = req.body;
     res.json({ message: `Hello, TypeScript Backend! ${prompt}` });
+});
+
+
+app.post("/chatcdp", async(req: Request, res: Response) => {
+    const {prompt} = req.body;
+
+    const promptResponse  = await communicateWithAgent(cdpAgent, cdpConfig, prompt);
+    console.log(promptResponse);
+
+    res.json({ message: promptResponse });
 });
 
 app.listen(PORT, () => {
